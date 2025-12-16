18775a7246967b810a73a78cbd70662f1f2531c0
monica <monica.ranshur@zedexinfo.com>
2025-12-16 16:32:42 +0530

    Banner Changes

--- components/banner/HeroBanner.tsx
+++ components/banner/HeroBanner.tsx
@@ -7,12 +7,12 @@ interface Props {
 export default function HeroBanner({ data }: Props)  {
   return (
     <section
-      className="bg-cover bg-center py-50"
+      className="bg-cover bg-center"
       style={{ backgroundImage: `url(${data.banner_background?.url || ''})` }}
     >
-      <div className="banner-overlay" >
-        <div className="text-center text-white container" >
-        <h1 className="text-4xl font-bold">{data.banner_title}</h1>
+      <div className="banner-overlay py-50" >
+        <div className=" text-white container" >
+        <h1 className="font-bold max-w-[500px]">{data.banner_title}</h1>
         <p className="text-lg mt-2">{data.banner_subtitle}</p>
       </div>
       </div>
