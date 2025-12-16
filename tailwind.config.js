5fc6db3a557bea30c79c09f8258eca32d5a6bcd1
monica <monica.ranshur@zedexinfo.com>
2025-12-16 12:55:15 +0530

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",        //  REQUIRED
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",        // (optional but recommended)
   ],
   theme: {
-    container:{
+    container: {
       center: true,
-      screens:{
-        sm: '1400px',
-        md: '1400px',
-        lg: '1400px',
-        xl: '1400px',
-      }, // centers the container   
+      screens: {
+        sm: "1400px",
+        md: "1400px",
+        lg: "1400px",
+        xl: "1400px",
+      },
     },
-
     extend: {
       colors: {
-        primary: '#0b1226',      
-        accent: '#ff8906',  
-        white:'#fff',
-        gold:'#A78E63',     // CTA color
+        primary: "#0b1226",
+        accent: "#ff8906",
+        white: "#fff",
+        gold: "#A78E63",
+        darkblue: "#122031",
+        ligherblue: "#1c2833",
       },
     },
   },
